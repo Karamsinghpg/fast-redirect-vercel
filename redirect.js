4810956e275f1677e4c810688c87670e4babcc64
Karamsinghpg <Karamsinghpg@gmail.com>
2025-05-18 17:08:23 +0530

    Delete redirect.js

--- redirect.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.writeHead(302, {
-    Location: 'https://amuletsourcemadden.com/jw48ycfzha?key=e071886b078172929808ba61a6ffa89d',
-  });
-  res.end();
-}
